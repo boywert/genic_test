dce58e50da4d13426b3a425c854587aac95491f8
Chaichalit Srisawat <boywert@gmail.com>
2019-03-07 20:40:30 +0000

    quick commit

--- main.c
+++ main.c
@@ -407,7 +407,7 @@ void displacement_fields(void)
 	      i = Local_x_start;
 	    if((Local_x_start == 0) && (Local_nx < Nmesh))
 	      if(i >= Nmesh -1)
-		i == 0;
+		i = 0;
 	    if(j == Nmesh)
 	      j = Nmesh - 1;
 	    if(k == Nmesh)

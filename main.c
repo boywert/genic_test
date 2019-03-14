105e61b038ca27dad1358303a58839e914352418
Chaichalit Srisawat <boywert@gmail.com>
2019-03-14 17:40:48 +0000

    quick commit

--- main.c
+++ main.c
@@ -460,7 +460,7 @@ void displacement_fields(void)
             printf("Line %d n = %d axes = %d vel = %g dis = %g\n",__LINE__,n,axes,vel,dis);
 	    P[n].Vel[axes] = vel;	    
 	    P[n].Disp[axes] = dis;
-         
+             printf("Line %d n = %d axes = %d\n",__LINE__,n,axes);
 	    
 	    if(dis > maxdisp)
 	      maxdisp = dis;
